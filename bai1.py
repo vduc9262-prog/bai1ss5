7f8f32a6437ae346814c2ade9982da75ba492735
Vuduc2907 <vduc9262@gmail.com>
2026-05-22 08:41:00 +0700

    Update bai1.py

--- bai1.py
+++ bai1.py
@@ -1,19 +1,23 @@
 branch_count = int(input("Nhập số lượng chi nhánh: "))
 month_count = 3
+result = ""
 
 
-for month in range(1, month_count + 1):
-    for branch in range(1, branch_count + 1):
-        revenue = int(input(f"Nhập doanh thu Chi nhánh {branch} ,tháng {month}: "))
+for  month in range(1, branch_count  + 1):
+    for branch in range(1,month_count + 1):
+        revenue = int(input(f"Nhập doanh thu Chi nhánh {month} ,tháng {branch}: "))
+    result += f'chi nhánh {branch}, tháng {month} : {revenue} triệu đồng. \n'
     print()
+    
+
+print(result)
 
 
-for month in range(1, month_count + 1):
-    for branch in range(1, branch_count + 1):
-        print(f"Chi nhánh {branch} ,tháng {month} : {revenue} triệu đồng")
 
-# Vì sao cách duyệt này khiến báo cáo không gom dữ liệu theo từng chi nhánh?
-# vì thiếu print() ở vòng lặp month 
+
+# # Vì sao cách duyệt này khiến báo cáo không gom dữ liệu theo từng chi nhánh?
+# # vì thiếu print() ở vòng lặp month 
+# tạo thêm 1 biến result cho nó += và in ra chi nhanh tháng và số tiền 
 
 # Vì vòng lặp ngoài là tháng, nên chương trình nhập và in theo tháng trước.
 
